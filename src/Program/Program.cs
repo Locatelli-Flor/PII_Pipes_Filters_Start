36fcb782c5f513b7cd722da9c97996dc2aa20743
Locatelli-Flor <floriana.locatelli@correo.ucu.edu.uy>
2022-10-23 21:49:25 -0300

    Parte 1, 2 y 3

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,21 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            FiltroGuardar Guardar1 = new FiltroGuardar(picture, @"guardado1.jpg");
+            FiltroGuardar Guardar2 = new FiltroGuardar(picture, @"guardado2.jpg");
+
+            PipeNull Nulo = new PipeNull();
+            PipeSerial Guardado2 = new PipeSerial(Guardar2, Nulo);
+            PipeSerial Serial2 = new PipeSerial(new FilterNegative(), Guardado2);
+            
+            PipeSerial Guardado1 = new PipeSerial(Guardar1, Serial2);
+            PipeSerial Serial1 = new PipeSerial(new FilterGreyscale(), Guardado1);
+            Serial1.Send(picture);
+
+
         }
     }
 }
